4beda4550f1e5deb43d661619f065468210819e2
HeavenDuke <trashlhc@163.com>
2017-04-28 11:52:38 +0800

    构造网站基本架构

--- crawler/libs/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by heavenduke on 17-4-2.
- */
-
-exports.RepositoryCrawler = require('./RepositoryCrawler');
-
-exports.UserCrawler = require('./UserCrawler');
--- database/models/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by heavenduke on 17-4-2.
- */
-
-exports.repository = require('./repository');
-
-exports.user = require('./user');
-
-exports.star = require('./star');
