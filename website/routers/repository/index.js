366e65d3e2fc1cc5b3bba8fdd7e32a24f3c29e4c
HeavenDuke <trashlhc@163.com>
2017-05-17 20:38:59 +0800

    开始调试star接口

--- website/routers/repository/index.js
+++ website/routers/repository/index.js
@@ -5,12 +5,11 @@
 var express = require('express');
 var repository_controller = require('../../controllers').repositories;
 var star_router = require('./star');
-var authentication = require('../../middlewares').authentication;
 var astepback = require('../../middlewares').astepback;
 var router = express.Router();
 
 router.get('/:owner/:name', astepback, repository_controller.show);
 
-router.use('/:owner/:name/star', authentication, star_router);
+router.use(star_router);
 
 module.exports = router;
--- website/routers/repository/star/index.js
+++ website/routers/repository/star/index.js
@@ -4,10 +4,11 @@
 
 var express = require('express');
 var star_controller = require('../../../controllers').repositories.stars;
+var authentication = require('../../../middlewares').authentication;
 var router = express.Router();
 
-router.post('/', star_controller.create);
+router.post('/:owner/:name/star', authentication, star_controller.create);
 
-router.delete('/', star_controller.destroy);
+router.delete('/:owner/:name/star', authentication, star_controller.destroy);
 
 module.exports = router;
